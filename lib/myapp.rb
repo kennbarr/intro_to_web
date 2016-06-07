ab1973cb1093bc6ab50c83a64ad11b92518d24f5
Elena <elena.vilimaite@gmail.com>
2016-06-07 12:07:56 +0100

    cat names in a form

--- lib/myapp.rb
+++ lib/myapp.rb
@@ -1,14 +1,18 @@
 require 'sinatra'
 
-get '/' do
+get '/random-cat' do
 	@cat_name = ["Mittens", "Milkshake", "Fuzzball"].sample
   erb(:index)
 end
-
+get '/named-cat' do
+	p params[:name]
+	@cat_name = params[:name]
+  erb(:index)
+end
 get '/secret' do
   'Shhhhh!'
 end
 
-get '/cat' do 
+get '/index' do 
 	'<img style="border: dotted 1px red; padding: 5px" src="https://placekitten.com/g/300/300" alt="Picture of a cat">'
 end
