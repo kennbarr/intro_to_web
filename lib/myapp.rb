d0886ba9e29406eb56c2ab96738230c664d7fe54
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-06-07 12:23:47 +0100

    Advanced to step 16

--- lib/myapp.rb
+++ lib/myapp.rb
@@ -4,7 +4,12 @@ get '/random-cat' do
 	@cat_name = ["Mittens", "Milkshake", "Fuzzball"].sample
   erb(:index)
 end
-get '/named-cat' do
+
+get '/name-cat' do
+	erb(:form)
+end
+
+post '/named-cat' do
 	p params[:name]
 	@cat_name = params[:name]
   erb(:index)
@@ -13,6 +18,6 @@ get '/secret' do
   'Shhhhh!'
 end
 
-get '/index' do 
+get '/index' do
 	'<img style="border: dotted 1px red; padding: 5px" src="https://placekitten.com/g/300/300" alt="Picture of a cat">'
 end
